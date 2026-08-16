2730ba1d58a69abdc036358539d3438a60c40745
djjawsyettiirl <122735603+djjawsyettiirl@users.noreply.github.com>
2026-08-15 18:19:40 -0700

    Bump Android test build for connection cleanup

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.jarvis.secured"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.5.6"
+        versionCode = 13
+        versionName = "0.5.7-test"
     }
 
     signingConfigs {
