2cb986b3e0d2c6aa0197bd75d030293670f4fe89
djjawsyettiirl <122735603+djjawsyettiirl@users.noreply.github.com>
2026-08-15 17:09:14 -0700

    Start route push when JARVIS enters foreground

--- android/app/src/main/java/com/jarvis/secured/JarvisApplication.kt
+++ android/app/src/main/java/com/jarvis/secured/JarvisApplication.kt
@@ -1,10 +1,29 @@
 package com.jarvis.secured
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
 
-class JarvisApplication : Application() {
+class JarvisApplication : Application(), Application.ActivityLifecycleCallbacks {
+    private var pushStarted = false
+
     override fun onCreate() {
         super.onCreate()
         RouteUpdateWorker.schedule(this)
+        registerActivityLifecycleCallbacks(this)
     }
+
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+        if (!pushStarted) {
+            pushStarted = true
+            runCatching { RoutePushService.start(this) }
+        }
+    }
+
+    override fun onActivityStarted(activity: Activity) = Unit
+    override fun onActivityResumed(activity: Activity) = Unit
+    override fun onActivityPaused(activity: Activity) = Unit
+    override fun onActivityStopped(activity: Activity) = Unit
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) = Unit
+    override fun onActivityDestroyed(activity: Activity) = Unit
 }
